8403f2125752a0ff9b8e500c14b8a3d7569b42e0
Luka <luka.luka.luka93@gmail.com>
2024-08-03 10:14:31 -0700

    pulled the each value from the joke data, entered into inner HTML, and then made a click event to the button that will trigger the function newJoke, which has getData()

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const joke = document.querySelector('#joke');
+const jokes = document.querySelector('#joke');
 const button = document.querySelector('#joke-btn');
 
 const getData = async function (){
     const response = await fetch('https://api.chucknorris.io/jokes/random');
     const data = await response.json()
-    console.log(data)
+    const joke = data.value;
+    jokes.innerHTML = `${joke}`;
 }
 getData()
 
 
-
+function newJoke (){
+    getData()
+}
 
 
 button.addEventListener("click", newJoke);
